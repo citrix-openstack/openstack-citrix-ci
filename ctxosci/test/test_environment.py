3f13115dbd44578efc323796d76fe3ae1db88f49
Mate Lakat <mate.lakat@citrix.com>
2014-02-21 12:29:32 +0000

    Reformat code

--- ctxosci/test/test_environment.py
+++ ctxosci/test/test_environment.py
@@ -1,4 +1,5 @@
 import unittest
+import textwrap
 
 from ctxosci import environment
 
@@ -6,15 +7,17 @@ from ctxosci import environment
 class TestEnvironment(unittest.TestCase):
     def test_env_value(self):
         change_ref = 'SOME_CHANGEREF'
-        env  = 'ZUUL_URL=https://review.openstack.org'
-        env += ' ZUUL_REF=SOME_CHANGEREF'
-        env += ' PYTHONUNBUFFERED=true'
-        env += ' DEVSTACK_GATE_TEMPEST=1'
-        env += ' DEVSTACK_GATE_TEMPEST_FULL=1'
-        env += ' DEVSTACK_GATE_VIRT_DRIVER=xenapi'
-        # Set gate timeout to 3 hours
-        env += ' DEVSTACK_GATE_TIMEOUT=180'
-        env += ' APPLIANCE_NAME=devstack'
-        env += ' ENABLED_SERVICES=g-api,g-reg,key,n-api,n-crt,n-obj,n-cpu,n-sch,horizon,mysql,rabbit,sysstat,dstat,pidstat,s-proxy,s-account,s-container,s-object,n-cond'
 
-        self.assertEquals(env.split(), environment.get_environment(change_ref))
+        expected_env = textwrap.dedent("""
+        ZUUL_URL=https://review.openstack.org
+        ZUUL_REF=SOME_CHANGEREF
+        PYTHONUNBUFFERED=true
+        DEVSTACK_GATE_TEMPEST=1
+        DEVSTACK_GATE_TEMPEST_FULL=1
+        DEVSTACK_GATE_VIRT_DRIVER=xenapi
+        DEVSTACK_GATE_TIMEOUT=180
+        APPLIANCE_NAME=devstack
+        ENABLED_SERVICES=g-api,g-reg,key,n-api,n-crt,n-obj,n-cpu,n-sch,horizon,mysql,rabbit,sysstat,dstat,pidstat,s-proxy,s-account,s-container,s-object,n-cond
+        """)
+
+        self.assertEquals(expected_env.split(), environment.get_environment(change_ref))
