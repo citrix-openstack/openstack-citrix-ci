fa7986e2d3f59f60cdb1e7e26c0df10c8a767450
Mate Lakat <mate.lakat@citrix.com>
2014-02-24 17:40:55 +0000

    temporary commit

--- osci/gerrit.py
+++ osci/gerrit.py
@@ -1,9 +1,20 @@
 from pygerrit import events
 
 
+class EventFilter(object):
+    def is_event_matching_criteria(self, event):
+        if event is None:
+            return False
+        return self._is_event_matching_criteria(event)
+
+
 class FakeClient(object):
-    def __init__(self):
+    def __init__(self, env):
+        env = env or dict()
         self.fake_events = []
+        self.host = env.get('gerrit_host')
+        self.port = env.get('gerrit_port')
+        self.user = env.get('gerrit_username')
 
     def fake_insert_event(self, event):
         self.fake_events.append(event)
@@ -21,21 +32,21 @@ class DummyFilter(object):
         return self.result
 
 
-class CommentMatcher(object):
+class CommentMatcher(EventFilter):
     def __init__(self, matcher):
         self.matcher = matcher
 
-    def is_event_matching_criteria(self, event):
+    def _is_event_matching_criteria(self, event):
         comment = event.comment
         if self.matcher.match(comment):
             return True
 
 
-class ChangeMatcher(object):
+class ChangeMatcher(EventFilter):
     def __init__(self, projects):
         self.projects = projects
 
-    def is_event_matching_criteria(self, event):
+    def _is_event_matching_criteria(self, event):
         if event.change.branch == "master":
             if event.change.project in self.projects:
                 return True
