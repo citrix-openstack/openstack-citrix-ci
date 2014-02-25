b87c275ddf8f29895631a39b9b38ebaa9cc9580d
Bob Ball <bob.ball@citrix.com>
2014-02-25 23:12:15 +0000

    Fix cron job paths

--- upload_ci_status.sh
+++ upload_ci_status.sh
@@ -3,13 +3,13 @@
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 
 date > /tmp/current_queue.txt
-osci-manage --list --states Running,Queued >> /tmp/current_queue.txt
+/usr/local/bin/osci-manage --list --states Running,Queued >> /tmp/current_queue.txt
 date > /tmp/recent_finished.txt
-osci-manage --list --states Collected,Finished --recent 24 >> /tmp/recent_finished.txt
+/usr/local/bin/osci-manage --list --states Collected,Finished --recent 24 >> /tmp/recent_finished.txt
 date > /tmp/all_failures.txt
-osci-manage --failures >> /tmp/all_failures.txt
+/usr/local/bin/osci-manage --failures >> /tmp/all_failures.txt
 
-key=`awk '/SFTP_KEY/{print $3}' osci/config.py | tr -d "'" | tr -d '"'`
+key=`awk '/SFTP_KEY/{print $3}' $TOP_DIR/osci/config.py | tr -d "'" | tr -d '"'`
 
 scp -i $key /tmp/current_queue.txt /tmp/all_failures.txt /tmp/recent_finished.txt \
     svcacct_openstack@int-ca.downloads.xensource.com:/var/www/html/ca.downloads.xensource.com/OpenStack/xenserver-ci/
