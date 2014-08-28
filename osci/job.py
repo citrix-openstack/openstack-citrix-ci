36205daa93e5880fd4149b4b0b1bce8de249cced
Bob Ball <bob.ball@citrix.com>
2014-08-28 10:54:47 +0100

    Fix bash command line

--- osci/job.py
+++ osci/job.py
@@ -148,7 +148,7 @@ class Job(db.Base):
         # For some reason invoking this immediately fails...
         time.sleep(5)
         utils.execute_command('ssh$-q$-o$BatchMode=yes$-o$UserKnownHostsFile=/dev/null$-o$StrictHostKeyChecking=no$-i$%s$%s@%s$'\
-                              'nohup bash "source workspace-cache/devstack-gate/functions.sh; tsfilter /home/jenkins/run_tests_env" < /dev/null > run_tests.log 2>&1 &'%(
+                              'nohup bash -c "source workspace-cache/devstack-gate/functions.sh; tsfilter /home/jenkins/run_tests_env" < /dev/null > run_tests.log 2>&1 &'%(
                 Configuration().NODE_KEY, Configuration().NODE_USERNAME, node_ip), '$')
         self.update(db, state=constants.RUNNING)
 
