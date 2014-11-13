68e21eb434f90e8181878f74d576ef7d501e72d3
Mate Lakat <mlakat@mlakat.hu>
2014-11-13 15:42:00 +0100

    Activate environment

--- upload_ci_status.sh
+++ upload_ci_status.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -eux
 
-TOP_DIR=$(cd $(dirname "$0") && pwd)
-
 CI_DIR=/tmp/ci_status
 
 [ -e $CI_DIR ] && rm -rf $CI_DIR
 mkdir -p $CI_DIR
 
+set +u
+. /opt/osci/env/bin/activate
+set -u
+
 date > $CI_DIR/current_queue.txt
-/usr/local/bin/osci-view list --states Running,Queued,Collecting >> $CI_DIR/current_queue.txt
+osci-view list --states Running,Queued,Collecting >> $CI_DIR/current_queue.txt
 date > $CI_DIR/recent_finished.txt
-/usr/local/bin/osci-view list --states Collected,Finished --recent 24 >> $CI_DIR/recent_finished.txt
+osci-view list --states Collected,Finished --recent 24 >> $CI_DIR/recent_finished.txt
 date > $CI_DIR/all_failures.txt
-/usr/local/bin/osci-view failures --recent 168 >> $CI_DIR/all_failures.txt
+osci-view failures --recent 168 >> $CI_DIR/all_failures.txt
 
 # Upload to the "status" container so the CDN will refresh every 15 minutes
-/usr/local/bin/osci-upload -c status $CI_DIR ci_status
+osci-upload -c status $CI_DIR ci_status
