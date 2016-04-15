8fd0218a3b3cbe647f514ba3f21e60b2c2030341
Huan Xie <huan.xie@citrix.com>
2016-04-14 22:33:05 -0700

    Change to neutron network

--- osci/environment.py
+++ osci/environment.py
@@ -12,7 +12,8 @@ def get_environment(project_name, change_ref, branch):
     DEVSTACK_GATE_TEMPEST=1
     DEVSTACK_GATE_TEMPEST_FULL=1
     DEVSTACK_GATE_VIRT_DRIVER=xenapi
-    DEVSTACK_GATE_TIMEOUT=180
+    DEVSTACK_GATE_TIMEOUT=300
     APPLIANCE_NAME=devstack
+    DEVSTACK_GATE_NEUTRON=1
     ENABLED_SERVICES=g-api,g-reg,key,n-api,n-crt,n-obj,n-cpu,n-sch,horizon,mysql,rabbit,sysstat,dstat,pidstat,s-proxy,s-account,s-container,s-object,n-cond
     """ % (change_ref, project_name, project_name, branch)).split()
